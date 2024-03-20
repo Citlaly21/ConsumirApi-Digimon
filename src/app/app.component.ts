0cdaf129c7315814315c8110cfc237cce3b733a2
Citlaly21 <cruzlisset87@gmail.com>
2024-03-20 09:00:29 -0600

    Trabajo digimon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DigimonesServices } from './digimones/digimones.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'digimon';
+export class AppComponent 
+{
+  digimones:any;
+
+  constructor(public digimon:DigimonesServices){}
+
+  ngOnInit()
+  {
+    this.digimon.getDigimones().subscribe(
+      (r) => {this.digimones = r; console.log(r)},
+      (e) => {console.error (e)}
+    )
+  }
+
 }
